c76b8d8dcee2343b8d82bcf1ccee70b811cae027
Sumanthkalluri0128 <163962409+Sumanthkalluri0128@users.noreply.github.com>
2025-05-27 12:54:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server, {
   cors: {
-    origin: ["http://127.0.0.1:5173"],
+    origin: ["https://mern-chat-frontend.netlify.app/"],
     method: ["GET", "POST"],
     Credentials: true,
   },
